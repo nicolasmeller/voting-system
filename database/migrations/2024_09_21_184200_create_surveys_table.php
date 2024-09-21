c05ec2e8d7ea97ca35f9c8bd3744d1ae9acc4566
Nicolas Meller Søndergaard <nicolasmeller@hotmail.dk>
2024-09-21 22:43:35 +0200

    Update 2024_09_21_184200_create_surveys_table.php

--- database/migrations/2024_09_21_184200_create_surveys_table.php
+++ database/migrations/2024_09_21_184200_create_surveys_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('surveys', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('password')->nullable();
             $table->string('description')->nullable();
             $table->timestamps();
         });
